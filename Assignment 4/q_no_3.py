826d51c2f7bcfd6ab25cd119ac6498f023ea72f5
surwaseomkar <surwaseomkar05@gmail.com>
2026-08-17 15:03:31 +0530

    Delete Assignment 4 directory

--- Assignment 4/q_no_3.py
+++
@@ -1,7 +0,0 @@
-#WAP to print sum of series upto n.
-
-n=int(input('enter number:'))
-sum=0
-for i in range(1,n+1):
-    sum=sum+i
-print('sum',sum)
--- Assignment 4/q_no_9.py
+++
@@ -1,7 +0,0 @@
-#WAP to print all numbers in a range divisible by a given number.
-
-num=int(input('enter number:'))
-d=int(input('enter number:'))
-for i in range(1,num+1):
-    if i % d==0:
-        print(i)
